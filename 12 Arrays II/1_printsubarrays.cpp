df98d942350d517dce0bbf4753cd8a61a91b0701
pranavsalunkhe71 <psalunkhe7771@gmail.com>
2026-09-05 14:52:34 +0530

    print subarray

--- 12 Arrays II/1_printsubarrays.cpp
+++ 12 Arrays II/1_printsubarrays.cpp
@@ -1,32 +1,60 @@
-/* subarrays is a contiguous part of an array */
+// /* subarrays is a contiguous part of an array */
+
+// #include<iostream>
+// using namespace std;
+
+// int subarray(int *arr ,int n){
+
+//     for (int start = 0 ; start <n ; start++){
+    
+//         for (int end = start ; end <n ; end++){
+//             // // cout<<"("<<start<<","<<end<<")";
+
+//             for (int start = start ; start<=end ; start++){
+//                 cout<<arr[start];
+
+//             }
+//             cout<<",";
+//         }
+    
+//         cout<<endl;
+//     }
+// }
+
+
+// int main() {
+    
+//     int arr[5] = {1,2,3,4,5};
+//     int n = sizeof(arr)/sizeof(int);
+
+//     subarray(arr,n);
+//     return 0;
+// }
+
+
+
 
 #include<iostream>
 using namespace std;
 
-int subarray(int *arr ,int n){
+void subarray (int *arr , int n ){
 
-    for (int start = 0 ; start <n ; start++){
-    
-        for (int end = start ; end <n ; end++){
-            // // cout<<"("<<start<<","<<end<<")";
 
-            for (int start = start ; start<=end ; start++){
-                cout<<arr[start];
+    for (int start = 0 ; start < n ; start++){
 
-            }
-            cout<<",";
+        for (int end = start ; end < n  ;end++){
+            cout<<"["<<start<<","<<end<<"],";
         }
-    
         cout<<endl;
     }
+
+
 }
 
-
 int main() {
-    
     int arr[5] = {1,2,3,4,5};
-    int n = sizeof(arr)/sizeof(int);
+    int n = sizeof(arr) / sizeof(int);
 
-    subarray(arr,n);
+    subarray(arr , n );
     return 0;
 }
